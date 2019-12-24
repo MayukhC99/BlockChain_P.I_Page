6b83a9f07da1343b84988a58b38c54868d6c38f6
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2019-12-24 08:04:52 +0530

    added buysell script

---
+++ js/script2.js
@@ -0,0 +1,23 @@
+//author @MayukhC99
+
+$(function(){
+	let buysell_btn = $('#buysell_button');
+	let buy= $('#buy');
+	let sell= $('#sell');
+	
+	buy.click(function(){
+		if(buysell_btn.hasClass('btn-danger')===true){
+			buysell_btn.removeClass('btn-danger');
+			buysell_btn.addClass('btn-success');
+			buysell_btn.text('Buy');
+		}
+	})
+	
+	sell.click(function(){
+		if(buysell_btn.hasClass('btn-success')===true){
+			buysell_btn.removeClass('btn-success');
+			buysell_btn.addClass('btn-danger');
+			buysell_btn.text('Sell');
+		}
+	})
+})
